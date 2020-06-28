f5dca0ab86954fcaa9e54db909607b28eecf900a
mauriciovillam <mecaurio11@gmail.com>
2020-06-28 19:17:55 -0300

    Remove unnecessary files, and progress on the website design

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -1,10 +1,8 @@
-import ArmyScenario from './spectacle/scenarios/army-scenario'
 import Spectacle from './spectacle/spectacle'
 import CelestialScenario from './spectacle/scenarios/celestial-scenario'
 
 const spectacle = new Spectacle([
     new CelestialScenario,
-    //new ArmyScenario
 ])
 
 spectacle.start()
--- src/assets/scripts/spectacle/scenarios/celestial-scenario.ts
+++ src/assets/scripts/spectacle/scenarios/celestial-scenario.ts
@@ -2,6 +2,7 @@ import Scenario from "../scenario"
 import Spectacle from '../spectacle'
 import ScenarioElement from '../element'
 import anime from 'animejs/lib/anime.es'
+import Parallax from 'parallax-js';
 
 export default class CelestialScenario implements Scenario {
     className = 'celestial';
@@ -23,8 +24,8 @@ export default class CelestialScenario implements Scenario {
         const parallaxAnimation = (duration: number) => {
             return {top: {
                 value: 0,
-                duration:3000 * duration,
-                delay:2500,
+                duration:2000 * duration,
+                delay:2000,
                 easing: () => {
                     return (Math.random() >= .5) ? 'easeInOutExpo' : 'easeInOutSine'
                 }
@@ -34,25 +35,51 @@ export default class CelestialScenario implements Scenario {
         this.elements = [
             new ScenarioElement(this, 'element--stars', cloudAnimation, {}),
             //new ScenarioElement(this, 'element--layer_11', parallaxAnimation, {}),
-            new ScenarioElement(this, 'element--layer_10', parallaxAnimation(1), {}),
-            new ScenarioElement(this, 'element--layer_9', parallaxAnimation(1.2), {}),
-            new ScenarioElement(this, 'element--layer_8', parallaxAnimation(1.5), {}),
-            new ScenarioElement(this, 'element--layer_7', parallaxAnimation(1.7), {}),
-            new ScenarioElement(this, 'element--layer_6', parallaxAnimation(2.8), {}),
-            new ScenarioElement(this, 'element--layer_5', parallaxAnimation(2.3), {}),
-            new ScenarioElement(this, 'element--layer_4', parallaxAnimation(2.4), {}),
-            new ScenarioElement(this, 'element--layer_3', parallaxAnimation(2.55), {}),
-            new ScenarioElement(this, 'element--layer_2', parallaxAnimation(2.65), {}),
-            new ScenarioElement(this, 'element--layer_1', parallaxAnimation(2.75), {})
+            new ScenarioElement(this, 'element--layer element--layer_10', parallaxAnimation(1), {}),
+            new ScenarioElement(this, 'element--layer element--layer_9', parallaxAnimation(1.2), {}),
+            new ScenarioElement(this, 'element--layer element--layer_8', parallaxAnimation(1.5), {}),
+            new ScenarioElement(this, 'element--layer element--layer_7', parallaxAnimation(1.7), {}),
+            new ScenarioElement(this, 'element--layer element--layer_6', parallaxAnimation(2.8), {}),
+            new ScenarioElement(this, 'element--layer element--layer_5', parallaxAnimation(2.3), {}),
+            new ScenarioElement(this, 'element--layer element--layer_4', parallaxAnimation(2.4), {}),
+            new ScenarioElement(this, 'element--layer element--layer_3', parallaxAnimation(2.55), {}),
+            new ScenarioElement(this, 'element--layer element--layer_2', parallaxAnimation(2.65), {}),
+            new ScenarioElement(this, 'element--layer element--layer_1', parallaxAnimation(2.75), {})
         ];
     }
 
     play(): void {
+        let index = 0;
         this.elements.forEach((element: ScenarioElement) => {
             const configuration = element.animation
             configuration.targets = element.domElement
+
+            if (element.className !== 'element--title') {
+                element.domElement.setAttribute('data-depth', (0.5 - (0.05 * index)).toString());
+            }
+
             anime(configuration)
+            index ++;
         })
+
+        anime({
+            targets: '#info',
+            opacity: 1,
+            delay: 7000,
+            duration: 4000
+        });
+        
+        // document.getElementsByTagName('body')[0].style.overflow = 'hidden'
+        // document.getElementsByTagName('html')[0].style.overflow = 'hidden'
+
+        setTimeout(() => {
+            document.getElementsByClassName('element--stars')[0].remove();
+
+            const parallaxInstance = new Parallax(this.spectacle.domElement);
+            parallaxInstance.friction(0.1, 0.1);
+            document.getElementsByTagName('body')[0].style.overflow = 'visible'
+            document.getElementsByTagName('html')[0].style.overflow = 'visible'
+        }, 11000);
     }
 
     hide(): void {
